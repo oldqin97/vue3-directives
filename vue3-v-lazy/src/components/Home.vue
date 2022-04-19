99ffde336b684f3d8a0aab0d5da1205a72c6f977
oldqin97 <oldqin1997@gmail.com>
2022-04-19 23:46:15 +0800

    add -

--- vue3-v-lazy/src/components/Home.vue
+++ vue3-v-lazy/src/components/Home.vue
@@ -1,13 +1,13 @@
 <!--
  * @Author: qin
  * @Date: 2022-04-18 01:48:53
- * @LastEditTime: 2022-04-19 23:38:05
- * @FilePath: \directive\vue3-v-lazy\src\components\Home.vue
+ * @LastEditTime: 2022-04-19 23:45:58
+ * @FilePath: \vue3-directives\vue3-v-lazy\src\components\Home.vue
  *  -> The best way to explain it is to do it
 -->
 <template>
   <div class="box">
-    <h1>This is home page</h1>
+    <h1>This is home page -</h1>
   </div>
 </template>
 
